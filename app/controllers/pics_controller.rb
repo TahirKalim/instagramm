81b4d13d3bfc0e227632abc487b3030532108344
tahirkalim <thrkalim@gmail.com>
2017-12-12 01:04:34 +0100

    Edit, Update and Destroy actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,35 +1,51 @@
-class PicsController < ApplicationController
-	
+ class PicsController < ApplicationController
 	before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@pics = Pic.all.order("created_at DESC")
 	end
- 	
- 	def show
- 		
- 	end
+
+	def show
+	end
 
 	def new
- 		@pic = Pic.new
- 	end
+		@pic = Pic.new
+	end
 
- 	def create
- 		@pic = Pic.new(pic_params)
+	def create
+		@pic = Pic.new(pic_params)
 
-	 	if @pic.save
+		if @pic.save
 			redirect_to @pic, notice: "Yesss! It was posted!"
 		else
 			render 'new'
 		end
 	end
- 
- 	private
- 
- 	def pic_params
- 		params.require(:pic).permit(:title, :description)
- 	end
- 	
- 	def find_pic
- 		@pic = Pic.find(params[:id])
- 	end
- end
+
+	def edit
+	end
+
+	def update
+		if @pic.update(pic_params)
+			redirect_to @pic, notice: "Congrats! Pic was updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pic.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:title, :description)
+	end
+
+	def find_pic
+		@pic = Pic.find(params[:id])
+	end
+	
+end
